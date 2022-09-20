c55cf67173e13ac3d758e0ab531612a8ac695aa8
Sasha Petelin <alexanderzr58@icloud.com>
2022-09-20 23:18:36 +0300

    Homework6_C#_Petelin

---
+++ Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите число b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b1+b2)/(k1-k2);
+double y = k1*x+b1;
+
+Console.WriteLine($"Точка пересечения двух прямых будет в точке с координатами ({x};{y})");
---
+++ Ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите количество чисел, которое планируется ввести: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine();
+Console.Write("Количество положительных чисел в массиве ");
+Console.WriteLine(GetCountPositive(array));
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int GetCountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
